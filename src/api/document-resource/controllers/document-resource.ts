ab547129bc520606e6623f16d534f6fee90bae17
Ratanazz <rathanachan55@gmail.com>
2026-02-09 14:47:38 +0700

    add document content type

---
+++ src/api/document-resource/controllers/document-resource.ts
@@ -0,0 +1,7 @@
+/**
+ * document-resource controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::document-resource.document-resource');
---
+++ src/api/document-resource/routes/document-resource.ts
@@ -0,0 +1,7 @@
+/**
+ * document-resource router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::document-resource.document-resource');
