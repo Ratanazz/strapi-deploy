4ea72cb27d48aec0eeb35a272e0282dc2bc956b4
Ratanazz <rathanachan55@gmail.com>
2026-02-23 09:31:58 +0700

    add config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '.ngrok-free.app',
+      '.ngrok-free.dev'
+    ]
+  }
+});
